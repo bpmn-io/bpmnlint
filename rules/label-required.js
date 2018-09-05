f2095bcdcce545e0ddbd364fde09dbcc056473c5
Nico Rehwaldt <git_nikku@nixis.de>
2018-09-05 17:33:51 +0200

    feat(rules): improve label-required rule
    * enforce labels on forking gateways only
    * enforce conditional flow labels
    * enforce participant / lane labels
    * ignore boundary event / sub-process labels
    * ignore data object labels
    
    Closes #11

--- rules/label-required.js
+++ rules/label-required.js
@@ -2,9 +2,10 @@
  * Rule is to impose the presence of a label for every flow node.
  */
 
-module.exports = utils => {
-  const { is } = utils;
-  const ERROR = 'Element is missing a label/name.';
+module.exports = function(utils) {
+
+  const is = utils.is;
+  const isAny = utils.isAny;
 
   function check(node, reporter) {
 
@@ -12,10 +13,54 @@ module.exports = utils => {
       return;
     }
 
-    if (is(node, 'bpmn:FlowNode') && !(node.name || '').trim().length) {
-      reporter.report(node.id, ERROR);
+    // ignore joining gateways
+    if (is(node, 'bpmn:Gateway') && !isForking(node)) {
+      return;
+    }
+
+    if (is(node, 'bpmn:BoundaryEvent')) {
+      return;
+    }
+
+    // ignore sub-processes
+    if (is(node, 'bpmn:SubProcess')) {
+      // TODO(nikku): better ignore expanded sub-processes only
+      return;
+    }
+
+    // ignore sequence flow without condition
+    if (is(node, 'bpmn:SequenceFlow') && !hasCondition(node)) {
+      return;
+    }
+
+    // ignore data objects and artifacts for now
+    if (isAny(node, [
+      'bpmn:FlowNode',
+      'bpmn:SequenceFlow',
+      'bpmn:Participant',
+      'bpmn:Lane'
+    ])) {
+
+      const name = (node.name || '').trim();
+
+      if (name.length === 0) {
+        reporter.report(node.id, 'is missing label/name');
+      }
     }
   }
 
   return { check };
 };
+
+
+// helpers ////////////////////////
+
+function isForking(node) {
+  const outgoing = node.outgoing || [];
+
+  return outgoing.length > 1;
+}
+
+function hasCondition(node) {
+  return node.conditionExpression;
+}
