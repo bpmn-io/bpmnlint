3ca3ecaf587fb1d9920bf3db0dd6e208c7e0c54e
Nico Rehwaldt <git_nikku@nixis.de>
2018-09-05 16:48:17 +0200

    test(rules): verify start-event-required behavior

---
+++ test/rules/start-event-required.js
@@ -0,0 +1,35 @@
+import RuleTester from '../../lib/testers/rule-tester';
+
+import rule from '../../rules/start-event-required';
+
+import {
+  readModdle
+} from '../../lib/testers/helper';
+
+
+RuleTester.verify('start-event-required', rule, {
+  valid: [
+    {
+      moddleElement: readModdle(__dirname + '/start-event-required/valid.bpmn')
+    },
+    {
+      moddleElement: readModdle(__dirname + '/start-event-required/valid-sub-process.bpmn')
+    }
+  ],
+  invalid: [
+    {
+      moddleElement: readModdle(__dirname + '/start-event-required/invalid.bpmn'),
+      report: {
+        id: 'Process',
+        message: 'is missing a start event'
+      }
+    },
+    {
+      moddleElement: readModdle(__dirname + '/start-event-required/invalid-sub-process.bpmn'),
+      report: {
+        id: 'SubProcess',
+        message: 'is missing a start event'
+      }
+    }
+  ]
+});
