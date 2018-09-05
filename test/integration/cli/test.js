ae30990e82896e7087e7d64f9eb35fca056bab73
Nico Rehwaldt <git_nikku@nixis.de>
2018-09-05 15:58:17 +0200

    test(cli): improve coverage
    * test valid diagram behavior
    * test invalid diagram behavior
    * verify exit code (1 on failure)
    * verify output

--- test/integration/cli/test.js
+++ test/integration/cli/test.js
@@ -1,13 +1,54 @@
 const execa = require('execa');
 
+const assert = require('assert');
+
+async function exec(...args) {
+
+  try {
+    return await execa(...args);
+  } catch (err) {
+    return err;
+  }
+}
+
+function verifyResult({ code, stdout }) {
+
+  return (result) => {
+
+    if (typeof code !== 'undefined') {
+      assert.equal(result.code, code);
+    }
+
+    if (typeof stdout !== 'undefined') {
+      assert.equal(result.stdout, stdout);
+    }
+  };
+}
+
 async function testAll() {
 
-  await execa('bpmnlint', [ 'diagram.bpmn' ]);
+  await exec('bpmnlint', [ 'diagram.bpmn' ]).then(verifyResult({
+    code: 0,
+    stdout: `
+found 0 errors and 0 warnings`
+  }));
 
-  await execa('bpmnlint', [ '-c', 'extends-builtin.json', 'diagram.bpmn' ]);
+  await exec('bpmnlint', [ 'diagram-invalid.bpmn' ]).then(verifyResult({
+    code: 1,
+    stdout: `
+rule: start-event-required
+error: Process_08k516a is missing a Start Event
 
-  await execa('bpmnlint', [ '-c', 'extends-external.json', 'diagram.bpmn' ]);
+rule: end-event-required
+error: Process_08k516a is missing an End Event
 
+found 2 errors and 0 warnings
+`
+  }));
+
+  await exec('bpmnlint', [ '-c', 'extends-builtin.json', 'diagram.bpmn' ]).then(verifyResult({ code: 0 }));
+
+  await exec('bpmnlint', [ '-c', 'extends-external.json', 'diagram.bpmn' ]).then(verifyResult({ code: 0 }));
 }
 
 
