feced8f34431675a706d0a3b33392cbdb40d1c83
Nico Rehwaldt <git_nikku@nixis.de>
2018-09-05 13:54:09 +0200

    feat(rules): add single-blank-start-event rule

---
+++ test/rules/single-blank-start-event.js
@@ -0,0 +1,44 @@
+import RuleTester from '../../lib/testers/rule-tester';
+
+import rule from '../../rules/single-blank-start-event';
+
+import {
+  readModdle
+} from '../../lib/testers/helper';
+
+
+RuleTester.verify('single-blank-start-event', rule, {
+  valid: [
+    {
+      moddleElement: readModdle(__dirname + '/single-blank-start-event/valid.bpmn')
+    },
+    {
+      moddleElement: readModdle(__dirname + '/single-blank-start-event/valid-sub-process.bpmn')
+    },
+    {
+      moddleElement: readModdle(__dirname + '/single-blank-start-event/valid-typed.bpmn')
+    },
+    {
+      moddleElement: readModdle(__dirname + '/single-blank-start-event/valid-typed-sub-process.bpmn')
+    },
+    {
+      moddleElement: readModdle(__dirname + '/single-blank-start-event/valid-scopes.bpmn')
+    }
+  ],
+  invalid: [
+    {
+      moddleElement: readModdle(__dirname + '/single-blank-start-event/invalid.bpmn'),
+      report: {
+        id: 'Process',
+        message: 'contains multiple blank start events'
+      }
+    },
+    {
+      moddleElement: readModdle(__dirname + '/single-blank-start-event/invalid-sub-process.bpmn'),
+      report: {
+        id: 'SubProcess',
+        message: 'contains multiple blank start events'
+      }
+    }
+  ]
+});
