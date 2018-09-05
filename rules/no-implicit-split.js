b5d51b1f41ec5016feb15d17426a306796de0295
Nico Rehwaldt <git_nikku@nixis.de>
2018-09-05 14:39:52 +0200

    feat(rules): add no-implicit-split rule

---
+++ rules/no-implicit-split.js
@@ -0,0 +1,46 @@
+// verify that no implicit split is modeled
+// starting from a task.
+//
+// users should model the parallel splitting gateway
+// explicitly instead.
+
+module.exports = function(utils) {
+
+  const isAny = utils.isAny;
+
+  function check(node, reporter) {
+
+    if (!isAny(node, [
+      'bpmn:Task',
+      'bpmn:Event'
+    ])) {
+      return;
+    }
+
+    const outgoing = node.outgoing || [];
+
+    const outgoingWithoutCondition = outgoing.filter((flow) => {
+      return !hasCondition(flow) && !isDefaultFlow(node, flow);
+    });
+
+    if (outgoingWithoutCondition.length > 1) {
+      reporter.report(node.id, 'must not split implicitly');
+    }
+  }
+
+  return {
+    check
+  };
+
+};
+
+
+// helpers /////////////////////////////
+
+function hasCondition(flow) {
+  return !!flow.conditionExpression;
+}
+
+function isDefaultFlow(node, flow) {
+  return node['default'] === flow;
+}
---
+++ test/rules/no-implicit-split.js
@@ -0,0 +1,35 @@
+import RuleTester from '../../lib/testers/rule-tester';
+
+import rule from '../../rules/no-implicit-split';
+
+import {
+  readModdle
+} from '../../lib/testers/helper';
+
+
+RuleTester.verify('no-implicit-split', rule, {
+  valid: [
+    {
+      moddleElement: readModdle(__dirname + '/no-implicit-split/valid.bpmn')
+    },
+    {
+      moddleElement: readModdle(__dirname + '/no-implicit-split/valid-default-conditional-flow.bpmn')
+    }
+  ],
+  invalid: [
+    {
+      moddleElement: readModdle(__dirname + '/no-implicit-split/invalid-event.bpmn'),
+      report: {
+        id: 'Element',
+        message: 'must not split implicitly'
+      }
+    },
+    {
+      moddleElement: readModdle(__dirname + '/no-implicit-split/invalid-task.bpmn'),
+      report: {
+        id: 'Element',
+        message: 'must not split implicitly'
+      }
+    }
+  ]
+});
