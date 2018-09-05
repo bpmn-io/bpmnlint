0b8ea08f42c22c13170ac4a8de53a55036e18a48
Nico Rehwaldt <git_nikku@nixis.de>
2018-09-05 11:19:36 +0200

    feat(rules): add no-inclusive-gateway rule

---
+++ test/rules/no-inclusive-gateway.js
@@ -0,0 +1,25 @@
+import RuleTester from '../../lib/testers/rule-tester';
+
+import rule from '../../rules/no-inclusive-gateway';
+
+import {
+  readModdle
+} from '../../lib/testers/helper';
+
+
+RuleTester.verify('no-inclusive-gateway', rule, {
+  valid: [
+    {
+      moddleElement: readModdle(__dirname + '/no-inclusive-gateway/valid.bpmn')
+    }
+  ],
+  invalid: [
+    {
+      moddleElement: readModdle(__dirname + '/no-inclusive-gateway/invalid.bpmn'),
+      report: {
+        id: 'Gateway',
+        message: 'has disallowed type <bpmn:InclusiveGateway>'
+      }
+    }
+  ]
+});
