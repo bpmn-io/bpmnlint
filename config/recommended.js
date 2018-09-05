d79993f632ccc7434903d2a333dd86349242c8db
Nico Rehwaldt <git_nikku@nixis.de>
2018-09-05 23:05:31 +0200

    chore(config): update recommended config

--- config/recommended.js
+++ config/recommended.js
@@ -1,7 +1,18 @@
 module.exports = {
   rules: {
-    'label-required': 'warn',
+    'conditional-flows': 'error',
+    'end-event-required': 'error',
+    'event-sub-process-typed-start-event': 'error',
+    'fake-join': 'warn',
+    'label-required': 'error',
+    'no-complex-gateway': 'error',
+    'no-disconnected': 'error',
+    'no-gateway-join-fork': 'error',
+    'no-implicit-split': 'error',
+    'no-inclusive-gateway': 'error',
+    'single-blank-start-event': 'error',
+    'single-event-definition': 'error',
     'start-event-required': 'error',
-    'end-event-required': 'error'
+    'sub-process-blank-start-event': 'error'
   }
 };
