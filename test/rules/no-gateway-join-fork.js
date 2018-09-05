578b65f3602b65539abf5cc8b521ad4083a430d8
Nico Rehwaldt <git_nikku@nixis.de>
2018-09-05 14:04:48 +0200

    feat(rules): add no-gateway-join-fork rule

---
+++ test/rules/no-gateway-join-fork.js
@@ -0,0 +1,31 @@
+import RuleTester from '../../lib/testers/rule-tester';
+
+import rule from '../../rules/no-gateway-join-fork';
+
+import {
+  readModdle
+} from '../../lib/testers/helper';
+
+
+RuleTester.verify('no-gateway-join-fork', rule, {
+  valid: [
+    {
+      moddleElement: readModdle(__dirname + '/no-gateway-join-fork/valid-fork.bpmn')
+    },
+    {
+      moddleElement: readModdle(__dirname + '/no-gateway-join-fork/valid-join.bpmn')
+    },
+    {
+      moddleElement: readModdle(__dirname + '/no-gateway-join-fork/valid-fork-join-task.bpmn')
+    }
+  ],
+  invalid: [
+    {
+      moddleElement: readModdle(__dirname + '/no-gateway-join-fork/invalid.bpmn'),
+      report: {
+        id: 'Gateway',
+        message: 'forks and joins'
+      }
+    }
+  ]
+});
