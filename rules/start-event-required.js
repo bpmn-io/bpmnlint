05924bc16371853ffd6675a3db2199e53f6381f0
Nico Rehwaldt <git_nikku@nixis.de>
2018-09-05 16:48:17 +0200

    test(rules): verify start-event-required behavior

--- rules/start-event-required.js
+++ rules/start-event-required.js
@@ -2,24 +2,30 @@
  * Rule is to impose the presence of a start event in the process.
  */
 
-module.exports = utils => {
-  const { is } = utils;
+module.exports = function(utils) {
 
-  const ERROR = 'is missing a Start Event';
+  const is = utils.is;
+  const isAny = utils.isAny;
 
   function hasStartEvent(node) {
+    const flowElements = node.flowElements || [];
+
     return (
-      (node.flowElements || []).filter(
-        node => node.$type !== 'String' && is(node, 'bpmn:StartEvent')
-      ).length > 0
+      flowElements.some(node => is(node, 'bpmn:StartEvent'))
     );
   }
 
   function check(node, reporter) {
-    if (is(node, 'bpmn:Process')) {
-      if (!hasStartEvent(node)) {
-        reporter.report(node.id, ERROR);
-      }
+
+    if (!isAny(node, [
+      'bpmn:Process',
+      'bpmn:SubProcess'
+    ])) {
+      return;
+    }
+
+    if (!hasStartEvent(node)) {
+      reporter.report(node.id, 'is missing a start event');
     }
   }
 
