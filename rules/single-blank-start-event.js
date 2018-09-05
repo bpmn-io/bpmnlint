1d856f2c9cc777ecf36698db850c7ab915682233
Nico Rehwaldt <git_nikku@nixis.de>
2018-09-05 13:54:09 +0200

    feat(rules): add single-blank-start-event rule

---
+++ rules/single-blank-start-event.js
@@ -0,0 +1,34 @@
+// verify that there exists only a single blank
+// start event per scope
+
+module.exports = function(utils) {
+
+  const is = utils.is;
+
+  function check(node, reporter) {
+
+    if (!is(node, 'bpmn:FlowElementsContainer')) {
+      return;
+    }
+
+    const blankStartEvents = node.flowElements.filter(function(flowElement) {
+
+      if (!is(flowElement, 'bpmn:Event')) {
+        return false;
+      }
+
+      const eventDefinitions = flowElement.eventDefinitions || [];
+
+      return eventDefinitions.length === 0;
+    });
+
+    if (blankStartEvents.length > 1) {
+      reporter.report(node.id, 'contains multiple blank start events');
+    }
+  }
+
+  return {
+    check
+  };
+
+};
