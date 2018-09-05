bc27f349e17aa936134b4e3f947613b1157a4a04
Nico Rehwaldt <git_nikku@nixis.de>
2018-09-05 11:18:50 +0200

    feat(rules): add no-complex-gateway rule

---
+++ test/rules/no-complex-gateway.js
@@ -0,0 +1,25 @@
+import RuleTester from '../../lib/testers/rule-tester';
+
+import rule from '../../rules/no-complex-gateway';
+
+import {
+  readModdle
+} from '../../lib/testers/helper';
+
+
+RuleTester.verify('no-complex-gateway', rule, {
+  valid: [
+    {
+      moddleElement: readModdle(__dirname + '/no-complex-gateway/valid.bpmn')
+    }
+  ],
+  invalid: [
+    {
+      moddleElement: readModdle(__dirname + '/no-complex-gateway/invalid.bpmn'),
+      report: {
+        id: 'Gateway',
+        message: 'has disallowed type <bpmn:ComplexGateway>'
+      }
+    }
+  ]
+});
