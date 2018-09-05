20bfa475bfd2a2e7b899bb64c7fd883d5b85ee2d
Nico Rehwaldt <git_nikku@nixis.de>
2018-09-05 11:05:29 +0200

    feat(rules): add conditional-flows rule
    Assert that all flows are either conditional or default
    for conditional forks around gateways and activities.

---
+++ test/rules/conditional-flows.js
@@ -0,0 +1,52 @@
+import RuleTester from '../../lib/testers/rule-tester';
+
+import rule from '../../rules/conditional-flows';
+
+import {
+  readModdle
+} from '../../lib/testers/helper';
+
+
+RuleTester.verify('conditional-flows', rule, {
+  valid: [
+    {
+      moddleElement: readModdle(__dirname + '/conditional-flows/valid-split.bpmn')
+    },
+    {
+      moddleElement: readModdle(__dirname + '/conditional-flows/valid-split-after-task.bpmn')
+    },
+    {
+      moddleElement: readModdle(__dirname + '/conditional-flows/valid-conditional-fork.bpmn')
+    }
+  ],
+  invalid: [
+    {
+      moddleElement: readModdle(__dirname + '/conditional-flows/invalid-fork-after-exclusive-gateway.bpmn'),
+      report: {
+        id: 'Flow_2',
+        message: 'is missing condition'
+      }
+    },
+    {
+      moddleElement: readModdle(__dirname + '/conditional-flows/invalid-fork-after-exclusive-gateway-default.bpmn'),
+      report: {
+        id: 'Flow_1',
+        message: 'is missing condition'
+      }
+    },
+    {
+      moddleElement: readModdle(__dirname + '/conditional-flows/invalid-fork-after-task.bpmn'),
+      report: {
+        id: 'Flow_1',
+        message: 'is missing condition'
+      }
+    },
+    {
+      moddleElement: readModdle(__dirname + '/conditional-flows/invalid-fork-after-task-default.bpmn'),
+      report: {
+        id: 'Flow_1',
+        message: 'is missing condition'
+      }
+    }
+  ]
+});
