cf53a3894b6bd821e18ddfc7bdaa8da857356a37
Nico Rehwaldt <git_nikku@nixis.de>
2018-09-19 10:08:30 +0200

    feat(linter): add config compilation support script
    This adds a helper which compiles a bpmnlint configuration to a
    JavaScript file that exposes the resolved config as well as a resolver
    to retrieve the cached rules.
    
    The generated script may later be used to include .bpmnlintrc files in
    browser consumeable applications:
    
    ```javascript
    import { config, resolver } from './.bpmnlintrc';
    
    ...
    ```
    
    (To achieve this, additional bundler support is required.)

---
+++ test/spec/support/compile-config-spec.js
@@ -0,0 +1,46 @@
+const { expect } = require('chai');
+
+import compileConfig from '../../../lib/support/compile-config';
+
+
+describe('support/compile-config', function() {
+
+  it('should import rules', async function() {
+
+    // when
+    const code = await compileConfig({
+      rules: {
+        'conditional-flows': 'error',
+        'single-blank-start-event': 'off'
+      }
+    });
+
+    // then
+    // imports enabled rule
+    expect(code).to.contain('import rule_0 from \'bpmnlint/rules/conditional-flows\'');
+    expect(code).to.contain('cache[\'bpmnlint/conditional-flows\'] = rule_0');
+
+    // does not import disabled rule
+    expect(code).not.to.contain('from \'bpmnlint/rules/single-blank-start-event\'');
+
+    // exports config and resolver
+    expect(code).to.contain('export { resolver, config };');
+
+    expect(code).to.contain('export default bundle;');
+  });
+
+
+  it('should resolve extends', async function() {
+
+    // when
+    const code = await compileConfig({
+      extends: 'bpmnlint:recommended'
+    });
+
+    // then
+    // bundles enabled rules
+    expect(code).to.contain('conditional-flows');
+    expect(code).to.contain('single-blank-start-event');
+  });
+
+});
