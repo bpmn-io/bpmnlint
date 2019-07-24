fdbcca0d49056a3785c06a56adf77863cadf6d2e
Nico Rehwaldt <git_nikku@nixis.de>
2019-07-24 13:08:53 +0200

    chore(project): bump to bpmn-moddle@6

--- test/integration/cli-spec.js
+++ test/integration/cli-spec.js
@@ -102,23 +102,24 @@ describe('cli', function() {
     test({
       cmd: [ 'bpmnlint', 'complex.bpmn' ],
       expect: {
-        code: 0,
+        code: 1,
         stderr: EMPTY,
         stdout: `
 
           ${diagramPath('complex.bpmn')}
-            sid-E391B624-F6E8-428B-9C3E-7026F85C4F24  warning  Element is missing label/name  label-required
-            sid-3E1FA189-AC8C-4CF1-9057-3D2EF8C6D3AF  warning  Element is missing label/name  label-required
-            sid-DD0BC4E1-4AA3-4835-A477-373EA263A593  warning  Element is missing label/name  label-required
-            sid-B8B18E3A-EF8D-4D19-B5CD-C666D39E2E0D  warning  Element is missing label/name  label-required
-            sid-994BB7B0-64D8-4DC4-B549-0758628F5A16  warning  Element is missing label/name  label-required
-            sid-0F208AE2-EC60-4823-9B6A-D062A91587F2  warning  Element is missing label/name  label-required
-            sid-B22F345F-EAB4-4D92-A810-C4B9AFCC4A6F  warning  Element is missing label/name  label-required
-            sid-4E447BCA-4A6B-4944-8A1C-1A184D6A95FD  warning  Element is missing label/name  label-required
-            sid-4A0FB2B3-2D67-46F7-ACB3-260FC62E3B5A  warning  Element is missing label/name  label-required
-            sid-13E4B11F-E8D5-434E-9AE3-A42ED2084731  warning  Element is missing label/name  label-required
+            sid-8D8BD39F-1B08-433F-8F93-A1FF7520BA8B  error    Import warning: unknown attribute <isCollection>
+            sid-E391B624-F6E8-428B-9C3E-7026F85C4F24  warning  Element is missing label/name                     label-required
+            sid-3E1FA189-AC8C-4CF1-9057-3D2EF8C6D3AF  warning  Element is missing label/name                     label-required
+            sid-DD0BC4E1-4AA3-4835-A477-373EA263A593  warning  Element is missing label/name                     label-required
+            sid-B8B18E3A-EF8D-4D19-B5CD-C666D39E2E0D  warning  Element is missing label/name                     label-required
+            sid-994BB7B0-64D8-4DC4-B549-0758628F5A16  warning  Element is missing label/name                     label-required
+            sid-0F208AE2-EC60-4823-9B6A-D062A91587F2  warning  Element is missing label/name                     label-required
+            sid-B22F345F-EAB4-4D92-A810-C4B9AFCC4A6F  warning  Element is missing label/name                     label-required
+            sid-4E447BCA-4A6B-4944-8A1C-1A184D6A95FD  warning  Element is missing label/name                     label-required
+            sid-4A0FB2B3-2D67-46F7-ACB3-260FC62E3B5A  warning  Element is missing label/name                     label-required
+            sid-13E4B11F-E8D5-434E-9AE3-A42ED2084731  warning  Element is missing label/name                     label-required
 
-          ✖ 10 problems (0 errors, 10 warnings)
+          ✖ 11 problems (1 error, 10 warnings)
         `
       }
     });
