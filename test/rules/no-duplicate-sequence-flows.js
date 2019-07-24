8a0ab8f713ddeee4a9f1d50576e0cc476593a65b
Nico Rehwaldt <git_nikku@nixis.de>
2019-07-24 11:38:33 +0200

    feat(rules): add no-duplicate-sequence-flows rule
    Closes #22

---
+++ test/rules/no-duplicate-sequence-flows.js
@@ -0,0 +1,73 @@
+import RuleTester from '../../lib/testers/rule-tester';
+
+import rule from '../../rules/no-duplicate-sequence-flows';
+
+import {
+  readModdle
+} from '../../lib/testers/helper';
+
+
+RuleTester.verify('no-duplicate-sequence-flows', rule, {
+  valid: [
+    {
+      moddleElement: readModdle(__dirname + '/no-duplicate-sequence-flows/valid.bpmn')
+    }
+  ],
+  invalid: [
+    {
+      moddleElement: readModdle(__dirname + '/no-duplicate-sequence-flows/invalid-no-condition.bpmn'),
+      report: [
+        {
+          id: 'SequenceFlow_2',
+          message: 'SequenceFlow is a duplicate'
+        },
+        {
+          id: 'StartEvent_1',
+          message: 'Duplicate outgoing sequence flows'
+        },
+        {
+          id: 'Task_2',
+          message: 'Duplicate incoming sequence flows'
+        }
+      ]
+    },
+    {
+      moddleElement: readModdle(__dirname + '/no-duplicate-sequence-flows/invalid-condition.bpmn'),
+      report: [
+        {
+          id: 'SequenceFlow_1',
+          message: 'SequenceFlow is a duplicate'
+        },
+        {
+          id: 'Task_1',
+          message: 'Duplicate outgoing sequence flows'
+        },
+        {
+          id: 'Task_2',
+          message: 'Duplicate incoming sequence flows'
+        }
+      ]
+    },
+    {
+      moddleElement: readModdle(__dirname + '/no-duplicate-sequence-flows/invalid-multiple.bpmn'),
+      report: [
+        {
+          id: 'SequenceFlow_2',
+          message: 'SequenceFlow is a duplicate'
+        },
+        {
+          id: 'Task_1',
+          message: 'Duplicate outgoing sequence flows'
+        },
+        {
+          id: 'Task_2',
+          message: 'Duplicate incoming sequence flows'
+        },
+        {
+          id: 'SequenceFlow_3',
+          message: 'SequenceFlow is a duplicate'
+        }
+      ]
+    }
+  ]
+});
