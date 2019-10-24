6920da9f5a6e7abe33a51c14722bead902cd39e3
Nico Rehwaldt <git_nikku@nixis.de>
2019-10-24 08:47:50 +0200

    feat(linter): rework scoped rules / configuration code
    * recognize both, short and long form of lint rules:
    
    ```
    {
      "rules": {
        "bpmnlint-plugin-foo/bar": "off"
        "foo/other": "warn"
      }
    }
    ```
    
    * keep the short form the default when displaying and identifying errors
    
    * recognize long and short form of scoped lint rules and configuration,
    too:
    
    {
      "rules": {
        "@ns/bpmnlint-plugin-foo/bar": "off"
        "@ns/foo/other": "warn"
      }
    }
    
    Related to #35

--- test/spec/support/compile-config-spec.js
+++ test/spec/support/compile-config-spec.js
@@ -30,6 +30,21 @@ describe('support/compile-config', function() {
   });
 
 
+  it('should import namespaced', async function() {
+
+    // when
+    const code = await compileConfig({
+      rules: {
+        '@foo/bar/rule': 'warn'
+      }
+    });
+
+    // then
+    expect(code).to.contain('import rule_0 from \'@foo/bpmnlint-plugin-bar/rules/rule\'');
+    expect(code).to.contain('cache[\'@foo/bpmnlint-plugin-bar/rule\'] = rule_0');
+  });
+
+
   it('should resolve extends', async function() {
 
     // when
