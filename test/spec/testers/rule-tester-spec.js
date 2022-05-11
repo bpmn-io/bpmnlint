b9e78644d6d86c481110a7b16680378504316b55
Philipp <philippfromme@outlook.com>
2022-05-11 16:07:30 +0200

    chore: comments

--- test/spec/testers/rule-tester-spec.js
+++ test/spec/testers/rule-tester-spec.js
@@ -21,9 +21,11 @@ describe('rule-tester', function() {
 
     it('should not be equal', function() {
 
+      // given
       const a = { foo: 'bar' };
       const b = { foo: 'baz' };
 
+      // when
       // then
       expect(() => expectEqual(a, b)).to.throw();
     });
@@ -31,9 +33,11 @@ describe('rule-tester', function() {
 
     it('should be equal (moddle element with ID)', function() {
 
+      // given
       const a = { node: moddle.create('bpmn:Task', { id: 'Task_1' }) };
       const b = { node: 'Task_1' };
 
+      // when
       // then
       expect(() => expectEqual(a, b)).not.to.throw();
     });
@@ -41,9 +45,11 @@ describe('rule-tester', function() {
 
     it('should be equal (moddle element with no ID)', function() {
 
+      // given
       const a = { node: moddle.create('bpmn:Task') };
       const b = { node: 'bpmn:Task' };
 
+      // when
       // then
       expect(() => expectEqual(a, b)).not.to.throw();
     });
