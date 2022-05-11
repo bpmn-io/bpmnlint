e9aeb236eb2f149105fbf9a1131d28c59270dcf3
Philipp <philippfromme@outlook.com>
2022-05-11 14:05:04 +0200

    feat(rule-tester): fuzzy test for equality of nodes

--- test/spec/testers/rule-tester-spec.js
+++ test/spec/testers/rule-tester-spec.js
@@ -1,9 +1,55 @@
-import { getTitle } from '../../../lib/testers/rule-tester';
+import BpmnModdle from 'bpmn-moddle';
+
+import {
+  expectEqual,
+  getTitle
+} from '../../../lib/testers/rule-tester';
 
 import { expect } from '../../helper';
 
 
 describe('rule-tester', function() {
+
+  let moddle;
+
+  beforeEach(function() {
+    moddle = new BpmnModdle();
+  });
+
+
+  describe('expectEqual', function() {
+
+    it('should not be equal', function() {
+
+      const a = { foo: 'bar' };
+      const b = { foo: 'baz' };
+
+      // then
+      expect(() => expectEqual(a, b)).to.throw();
+    });
+
+
+    it('should be equal (moddle element with ID)', function() {
+
+      const a = { node: moddle.create('bpmn:Task', { id: 'Task_1' }) };
+      const b = { node: 'Task_1' };
+
+      // then
+      expect(() => expectEqual(a, b)).not.to.throw();
+    });
+
+
+    it('should be equal (moddle element with no ID)', function() {
+
+      const a = { node: moddle.create('bpmn:Task') };
+      const b = { node: 'bpmn:Task' };
+
+      // then
+      expect(() => expectEqual(a, b)).not.to.throw();
+    });
+
+  });
+
 
   describe('#getTitle', function() {
 
