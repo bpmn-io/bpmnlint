725bc13bf5d4eab7097c4b09889ccbe308b1a068
Nico Rehwaldt <git_nikku@nixis.de>
2023-06-13 12:53:39 +0200

    fix(rules/no-implicit-start): properly handle additional patterns
    * Boundary events
    * Event sub-processes
    * Link catch events
    
    Closes https://github.com/bpmn-io/bpmnlint/issues/105

--- rules/no-implicit-start.js
+++ rules/no-implicit-start.js
@@ -9,10 +9,30 @@ const {
  */
 module.exports = function() {
 
+  function isLinkEvent(node) {
+    const eventDefinitions = node.eventDefinitions || [];
+
+    return eventDefinitions.length && eventDefinitions.every(
+      definition => is(definition, 'bpmn:LinkEventDefinition')
+    );
+  }
+
   function isImplicitStart(node) {
     const incoming = node.incoming || [];
 
-    return !is(node, 'bpmn:StartEvent') && incoming.length === 0;
+    if (is(node, 'bpmn:SubProcess') && node.triggeredByEvent) {
+      return false;
+    }
+
+    if (is(node, 'bpmn:IntermediateCatchEvent') && isLinkEvent(node)) {
+      return false;
+    }
+
+    if (isAny(node, [ 'bpmn:StartEvent', 'bpmn:BoundaryEvent' ])) {
+      return false;
+    }
+
+    return incoming.length === 0;
   }
 
   function check(node, reporter) {
