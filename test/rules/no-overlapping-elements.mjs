68273efd3b5d0817cff7706c98c420b8d89dbf8d
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2024-05-24 15:23:42 +0200

    test(rules/no-overlapping-elements): verify missing DI handling

--- test/rules/no-overlapping-elements.mjs
+++ test/rules/no-overlapping-elements.mjs
@@ -29,6 +29,9 @@ RuleTester.verify('no-overlapping-elements', rule, {
     },
     {
       moddleElement: readModdle(__dirname + '/no-overlapping-elements/valid-subprocess-collapsed.bpmn')
+    },
+    {
+      moddleElement: readModdle(__dirname + '/no-overlapping-elements/ignore-missing-di.bpmn')
     }
   ],
   invalid: [
