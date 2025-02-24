5e1b162775db7b1b4f8e7eab0bceb74c14687682
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2025-02-24 14:34:14 +0100

    feat: add `correctness` rule set
    Closes https://github.com/bpmn-io/bpmnlint/issues/170

---
+++ config/correctness.js
@@ -0,0 +1,9 @@
+module.exports = {
+  rules: {
+    'ad-hoc-sub-process': 'error',
+    'event-sub-process-typed-start-event': 'error',
+    'link-event': 'error',
+    'no-duplicate-sequence-flows': 'warn',
+    'sub-process-blank-start-event': 'error'
+  }
+};
--- test/spec/config-spec.mjs
+++ test/spec/config-spec.mjs
@@ -3,6 +3,7 @@ import { expect } from 'chai';
 import fs from 'node:fs';
 
 import allConfig from 'bpmnlint/config/all.js';
+import correctnessConfig from 'bpmnlint/config/correctness.js';
 
 
 describe('config', function() {
@@ -25,6 +26,29 @@ describe('config', function() {
 
       // then
       expect(configuredRules).to.eql(definedRules);
+    });
+
+  });
+
+
+  describe('correctness', function() {
+
+    it('should contain relevant lint rules', function() {
+
+      // given
+      const expectedRules = [
+        'ad-hoc-sub-process',
+        'event-sub-process-typed-start-event',
+        'link-event',
+        'no-duplicate-sequence-flows',
+        'sub-process-blank-start-event'
+      ];
+
+      // when
+      const configuredRules = Object.keys(correctnessConfig.rules);
+
+      // then
+      expect(configuredRules).to.eql(expectedRules);
     });
 
   });
