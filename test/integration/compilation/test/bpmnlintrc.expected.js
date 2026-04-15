3269214deb325c1d3e3acdabc64ae16408b0231f
Philipp Fromme <philipp.fromme@camunda.com>
2026-04-15 13:55:53 +0200

    fix: change no-inclusive-gateway to warn

--- test/integration/compilation/test/bpmnlintrc.expected.js
+++ test/integration/compilation/test/bpmnlintrc.expected.js
@@ -46,7 +46,7 @@ const rules = {
   "no-implicit-split": "error",
   "no-implicit-end": "error",
   "no-implicit-start": "error",
-  "no-inclusive-gateway": "error",
+  "no-inclusive-gateway": "warn",
   "no-overlapping-elements": "warn",
   "single-blank-start-event": "error",
   "single-event-definition": "error",
