e42c65d808fea8d3b082fdd6ca034504a0d77148
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2026-08-19 12:03:41 +0200

    test: update webpack bundling fixture

--- test/integration/bundling/test/app.webpack.expected.js
+++ test/integration/bundling/test/app.webpack.expected.js
@@ -513,17 +513,17 @@ module.exports = /*#__PURE__*/JSON.parse('{"name":"Spell","uri":"http://example.
 /******/   });
 /************************************************************************/
 /******/   // The module cache
-/******/   var __webpack_module_cache__ = {};
+/******/   const __webpack_module_cache__ = {};
 /******/   
 /******/   // The require function
 /******/   function __webpack_require__(moduleId) {
 /******/     // Check if module is in cache
-/******/     var cachedModule = __webpack_module_cache__[moduleId];
+/******/     const cachedModule = __webpack_module_cache__[moduleId];
 /******/     if (cachedModule !== undefined) {
 /******/       return cachedModule.exports;
 /******/     }
 /******/     // Create a new module (and put it into the cache)
-/******/     var module = __webpack_module_cache__[moduleId] = {
+/******/     const module = __webpack_module_cache__[moduleId] = {
 /******/       // no module.id needed
 /******/       // no module.loaded needed
 /******/       exports: {}
@@ -532,7 +532,7 @@ module.exports = /*#__PURE__*/JSON.parse('{"name":"Spell","uri":"http://example.
 /******/     // Execute the module function
 /******/     if (!(moduleId in __webpack_modules__)) {
 /******/       delete __webpack_module_cache__[moduleId];
-/******/       var e = new Error("Cannot find module '" + moduleId + "'");
+/******/       const e = new Error("Cannot find module '" + moduleId + "'");
 /******/       e.code = 'MODULE_NOT_FOUND';
 /******/       throw e;
 /******/     }
@@ -547,7 +547,7 @@ module.exports = /*#__PURE__*/JSON.parse('{"name":"Spell","uri":"http://example.
 /******/   (() => {
 /******/     // getDefaultExport function for compatibility with non-harmony modules
 /******/     __webpack_require__.n = (module) => {
-/******/       var getter = module && module.__esModule ?
+/******/       const getter = module && module.__esModule ?
 /******/         () => (module['default']) :
 /******/         () => (module);
 /******/       __webpack_require__.d(getter, { a: getter });
@@ -557,11 +557,26 @@ module.exports = /*#__PURE__*/JSON.parse('{"name":"Spell","uri":"http://example.
 /******/   
 /******/   /* webpack/runtime/define property getters */
 /******/   (() => {
-/******/     // define getter functions for harmony exports
+/******/     // define getter/value functions for harmony exports
 /******/     __webpack_require__.d = (exports, definition) => {
-/******/       for(var key in definition) {
-/******/         if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
-/******/           Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/       if(Array.isArray(definition)) {
+/******/         var i = 0;
+/******/         while(i < definition.length) {
+/******/           var key = definition[i++];
+/******/           var binding = definition[i++];
+/******/           if(!__webpack_require__.o(exports, key)) {
+/******/             if(binding === 0) {
+/******/               Object.defineProperty(exports, key, { enumerable: true, value: definition[i++] });
+/******/             } else {
+/******/               Object.defineProperty(exports, key, { enumerable: true, get: binding });
+/******/             }
+/******/           } else if(binding === 0) { i++; }
+/******/         }
+/******/       } else {
+/******/         for(var key in definition) {
+/******/           if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/             Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/           }
 /******/         }
 /******/       }
 /******/     };
@@ -576,7 +591,7 @@ module.exports = /*#__PURE__*/JSON.parse('{"name":"Spell","uri":"http://example.
 /******/   (() => {
 /******/     // define __esModule on exports
 /******/     __webpack_require__.r = (exports) => {
-/******/       if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/       if(Symbol.toStringTag) {
 /******/         Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
 /******/       }
 /******/       Object.defineProperty(exports, '__esModule', { value: true });
@@ -584,7 +599,7 @@ module.exports = /*#__PURE__*/JSON.parse('{"name":"Spell","uri":"http://example.
 /******/   })();
 /******/   
 /************************************************************************/
-var __webpack_exports__ = {};
+let __webpack_exports__ = {};
 // This entry needs to be wrapped in an IIFE because it needs to be in strict mode.
 (() => {
 "use strict";
